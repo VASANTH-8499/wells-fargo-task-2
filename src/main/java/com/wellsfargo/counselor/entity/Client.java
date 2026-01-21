b83533d42aa4e66d5963c2436861a185ebae4daa
vasanth <vasanth@vasu.com>
2026-01-21 13:41:06 +0530

    Added Client, Portfolio, and Security JPA entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,82 @@
+
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    protected Client() {
+    }
+
+    public Client(String firstName, String lastName, String email, Advisor advisor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.advisor = advisor;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
